80189f792a808778152febec8869a1b9115f50c1
Eunji Jung <bian87@dgu.ac.kr>
2023-08-24 23:41:13 +0900

    [FEAT] #13 Add simple inspection functionality > > 회원가입 id, email, pw, call 입력 시 필요한 간단한 유효성 검사 추가 > 값 입력 후 서버로 초큰 전송 기능 추가

--- hello-wim/src/components/Signup.js
+++ hello-wim/src/components/Signup.js
@@ -1,12 +1,35 @@
-import React, { useState } from 'react';
-import './Signin.css'; 
+import React, { useState, useEffect } from 'react';
+import './signup.css'; 
 
 const Signup = () => {
   const [id, setId] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [callnumber, setCallnumber] = useState('');
+  const [emailCode, setEmailCode] = useState('');
+  const [token, setToken] = useState('');
   const [errorMsg, setErrorMsg] = useState('');
+
+  //간단한 유효성 검사 함수
+  const isValid = () => {
+    if(id.length < 5) {
+      setErrorMsg('ID는 5글자 이상이어야 합니다.');
+      return false;
+    }
+    if (!email.includes('@')) {
+      setErrorMsg('허용되는 이메일 양식이 아닙니다.');
+      return false;
+    }
+    if (password.length < 8){
+      setErrorMsg('비밀번호는 9글자 이상이어야 합니다.');
+      return false;
+    }
+    if(callnumber.length !== 10) {
+      setErrorMsg('올바른 전화번호를 입력해주십시오.');
+      return false;
+    }
+    return true;
+  }
 
   const fakeAPIRequest = () => {
     // 간단한 시뮬레이션: ID가 'admin'이면 실패로 간주하고, 그렇지 않으면 성공으로 간주
@@ -16,33 +39,60 @@ const Signup = () => {
     return true;
   };
 
-  const handleSignup = (e) => {
-    e.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-    // 이 값을 사용하여 서버에 회원가입 요청을 보낼 수 있음
-    console.log({
-      id,
-      email,
-      password,
-      callnumber
-    });
+        // 객체를 생성하여 아이디와 비밀번호 정보를 담음
+        const SignupData = {
+            id: id,
+            password: password,
+            email: email,
+            callnumber: callnumber
+        };
 
-//회원가입 성공 실패 여부
-    if (fakeAPIRequest()) {
-        setErrorMsg('Successfully registered!');
-        // 여기에 추가적인 로직(로그인 페이지로 리디렉션 등)을 추가할 수 있음
+        try { //로그인 표청 시작
+            // 서버에 POST 요청 보내기
+            const response = await fetch('http://localhost:3000/signin', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(SignupData) //사용자 입력갑 JSON 형식 문자열로 반환 후 본문에 포함시킴.
+            });
+
+            // 서버 응답 처리
+            if (response.ok) {
+              const data = await response.json();
+        // 3번: 클라이언트에서의 처리 - 서버에서 반환된 토큰을 상태와 localStorage에 저장
+        setToken(data.token);
+        localStorage.setItem('authToken', data.token);
+        //유효성 검사
+    if(!isValid()) return;
+
+    //회원가입 성공 실패 여부
+        if (fakeAPIRequest()) {
+            setErrorMsg('성공하였습니다.');
+            // 여기에 추가적인 로직(로그인 페이지로 리디렉션 등)을 추가할 수 있음
+          } 
       } else {
-        setErrorMsg('Registration failed. Please try again.');
-      }
-  };
+                // 로그인 실패 처리
+                console.error('로그인 실패');
+                setErrorMsg('실패하였습니다. 다시 시도래주세요. ');
+            }
+        } catch (error) {
+            console.error('서버 요청 오류:', error);
+            setErrorMsg('서버 요청 오류');
+        }
+    };
+
 
   return (
     <main className="sign-container">
       <h1 style={{fontSize: '50px'}}>WIM</h1>
 
       <div className="sign-Box">
-        <h2 style={{color: 'black', marginBottom: '4rem'}}>Signup</h2>
-        <form onSubmit={handleSignup}>
+        <h2 style={{color: 'black', marginBottom: '3rem'}}>Signup</h2>
+        <form onSubmit={handleSubmit}>
           <div>
           <label htmlFor="Id" />
             <input
@@ -79,6 +129,16 @@ const Signup = () => {
               placeholder="Call Number"
               value={callnumber}
               onChange={(e) => setCallnumber(e.target.value)}
+            />
+             <label htmlFor="EmailCode"></label>
+            <input
+              className="SignEmailCode"
+              type="text"
+              id="EmailCode"
+              name="EmailCode"
+              placeholder="Enter verification code sent to your email"
+              value={emailCode}
+              onChange={(e) => setEmailCode(e.target.value)}
             />
           </div>
 
