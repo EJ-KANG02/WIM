e5a20f527bae2a35e36ce865da1ffeb17c6768b6
Eunji Jung <bian87@dgu.ac.kr>
2023-08-26 23:50:08 +0900

    [FEAT] #15 Add newportfolio page
    새 포트폴리오 페이지 제작 및 링크 활성화 함.
    페이지 menubar, banner, 로고만 제작.
    추후 사용자 글 작성 및 사용자 입력 추가에 따른 item 추가 기능 제작 예정.
    css 파일 따로 만들어야함.(현재 main.css 같이 사용 중. 근데 어떻게 같이 사용할 수 있는건지 모르겠다.)

--- hello-wim/src/App.js
+++ hello-wim/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Main from './Main'
 import Signin from './components/Signin'
 import Signup from './components/Signup'
+import Newportfolio from './Newportfolio';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
     <Routes>
       <Route path="/signin" element={<Signin />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/Newportfolio" element={<Newportfolio />} />
       <Route exact path="/" element={<Main />} />
 
     </Routes>
---
+++ hello-wim/src/Newportfolio.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Newportfolio = () => {
+  return (
+    <div className="main-container">
+         <div className="Banner">
+            <div className="Introduce">
+                <h1 style={{fontSize: '50px'}}>MyPortFolio</h1>
+                <form>
+                    자기소개
+                </form>
+                <form>
+                    영문이름
+                </form>
+            </div>
+         </div> 
+         <div className="Main">
+        <div className="MenuBar">
+          {/* menuList */}
+
+        </div>
+        <div className="Content">
+          {/* Content */}
+          
+        </div>
+    </div>
+</div>
+  )
+}
+
+export default Newportfolio
