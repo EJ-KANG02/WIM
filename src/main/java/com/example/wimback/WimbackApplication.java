780f371c7799fec74535f1994a67b2973b45d40a
eunji <enji4@naver.com>
2023-09-09 23:51:27 +0900

    [REFACTOR] gitignore 등록

---
+++ src/main/java/com/example/wimback/WimbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.wimback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WimbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WimbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wimback/WimbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wimback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WimbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
